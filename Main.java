38a981db846b488e50797bca2561042567be6da8
PhichaPrim <phichamon_duangmanee@cmu.ac.th>
2024-12-18 16:41:11 +0700

    HW FIN

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        User john = new User("John", 1954, 12, 18);
+        john.displayInfo();
+        john.displayHappyBirthday();
+        System.out.println();
+        Admin nicolas = new Admin("Nicolas", 1964, 12, 18);
+        nicolas.displayInfo();
+        nicolas.displayHappyBirthday();
+    }
+}
